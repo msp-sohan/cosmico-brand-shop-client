3b8038114b04af5dc92769de4d58e3b598309a72
mspsohan <mspsohan@gmail.com>
2023-10-17 23:26:49 +0600

    React Basic Project Setup for Client side

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Cosmico Brand Shop</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+   return (
+      <div className="flex justify-center items-center h-screen">
+         <h1>This is a Error Page</h1>
+      </div>
+   );
+};
+
+export default ErrorPage;
